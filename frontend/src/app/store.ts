f4daf2e171c73329822d9151f18152b8f51ece8a
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2025-11-16 22:56:04 +0600

    store folder store code remove and code update

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -2,9 +2,16 @@ import { configureStore } from '@reduxjs/toolkit'
 import devicesReducer from '@/features/devices/deviceSlice'
 import presetsReducer from '@/features/presets/presetsSlice'
 
-export default configureStore({
+const store = configureStore({
   reducer: {
     devices: devicesReducer,
     presets: presetsReducer,
   },
 })
+
+// Infer the RootState and AppDispatch types
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+// Default export
+export default store
